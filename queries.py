07bca61d49ea3423f3857048badb931b0d663660
Botafogo1894 <pruchka@gmail.com>
2018-11-06 16:00:15 -0500

    finished

--- queries.py
+++ queries.py
@@ -1,13 +1,19 @@
 from models import *
+from sqlalchemy import create_engine
 
-def return_christian_bales_roles(session):
-    pass
+engine = create_engine('sqlite:///hollywood.db')
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def return_christian_bales_roles():
+    return session.query(Actor).filter(Actor.name == "Christian Bale").first().roles
     # Return a list of Christian Bale role instances
 
-def return_catwoman_actors(session):
-    pass
+def return_catwoman_actors():
+    return session.query(Role).filter(Role.character == "Catwoman").first().actors
     #  Return a list of actor instances that have played Catwoman
 
-def return_number_of_batman_actors(session):
-    pass
+def return_number_of_batman_actors():
+    return len(session.query(Role).filter(Role.character == "Batman").first().actors)
     # Return the number of actors that have played Batman
